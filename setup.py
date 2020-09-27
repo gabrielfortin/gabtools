64933e2afc3070c65ba9add46677d52ec1fe7a16
GabrielFortin <gabfortin@icloud.com>
2020-09-27 05:50:35 +0000

    Bumped version to 0.1.7: Now on PiP

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 import setuptools
 setup(name='GabTools',
-      version='0.1.6',
+      version='0.1.7',
       description='Diverse Python Utilities',
       author='Gabriel Fortin',
       author_email='gabriel.fortin97@gmail.com',
