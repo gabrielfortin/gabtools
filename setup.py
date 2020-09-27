2de03e762189cb997360c2c1ee3fe7e303e1f18a
GabrielFortin <gabfortin@icloud.com>
2020-09-27 03:41:49 +0000

    Changes to enable packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='GabTools',
+      version='0.1',
+      description='Diverse Python Utilities',
+      author='Gabriel Fortin',
+      author_email='gabriel.fortin97@gmail.com',
+      url='https://www.github.com/gabrielfortin/gabtools',
+      packages=['gabtools'],
+      package_dir={'':'.'}
+     )
